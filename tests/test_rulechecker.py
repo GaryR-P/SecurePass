9ae77b5d5fc4ca7487ebeb3961069b014233b3c6
GaryR-P <grose-petruck@uri.edu>
2025-05-20 19:42:28 -0400

    Add unit tests for rulechecker.py

---
+++ tests/test_rulechecker.py
@@ -0,0 +1,30 @@
+import unittest
+from rulechecker import check_password
+
+class TestRuleChecker(unittest.TestCase):
+    def test_valid_password(self):
+        self.assertTrue(check_password("StrongPass123!"))
+
+    def test_too_short(self):
+        self.assertFalse(check_password("Short1!"))
+
+    def test_missing_uppercase(self):
+        self.assertFalse(check_password("nouppercase123!"))
+
+    def test_missing_digit(self):
+        self.assertFalse(check_password("NoDigitsHere!"))
+
+    def test_missing_special(self):
+        self.assertFalse(check_password("NoSpecial123"))
+
+    def test_repeating_chars(self):
+        self.assertFalse(check_password("aaaBBB123!"))  # triple "a"
+
+    def test_sequential_digits(self):
+        self.assertFalse(check_password("abc123DEF!"))
+
+    def test_keyboard_sequence(self):
+        self.assertFalse(check_password("qwerty123!A"))
+
+if __name__ == '__main__':
+    unittest.main()
